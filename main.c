df1263acaa1c607a443e3b48ef66a15105c1aec4
Nguyen Nam Phong <phongnn57@gmail.com>
2015-05-06 16:02:36 +0700

    add keyboard + mouse function

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <GL/glut.h>
 #include<stdio.h>
 #include "pieces.h"
+#include <windows.h>
+#include <stdbool.h>
+
 float angle= 0;
 float i;
 int k=0;
@@ -11,6 +14,13 @@ int whitepawn[][2]={{1,0},{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7}};
 int whitepowers[][3]={{0,0,1},{0,1,2},{0,2,3},{0,3,4},{0,4,5},{0,5,3},{0,6,2},{0,7,1}};
 int blackpawn[][2]={{6,0},{6,1},{6,2},{6,3},{6,4},{6,5},{6,6},{6,7}};
 int blackpowers[][3]={{7,0,1},{7,1,2},{7,2,3},{7,3,4},{7,4,5},{7,5,3},{7,6,2},{7,7,1}};
+
+int mouseButton;
+int oldX, newX, oldY, newY;
+bool rotateAxisX = false;
+float rotateAngle = 0.0;
+float depth;
+
 void Keypress(unsigned char key,int x,int y)
 {
 	switch(key)
@@ -18,7 +28,31 @@ void Keypress(unsigned char key,int x,int y)
 		case 27:
 			exit(0);
 	}
+	
+
 }
+
+void SpecialFuncKey(int key, int x, int y) {
+	switch (key) {
+		case GLUT_KEY_UP:
+			rotateAngle += 10 * 0.11;
+			rotateAxisX = true;
+			break;
+		case GLUT_KEY_DOWN:
+			rotateAngle -= 10 * 0.11;
+			rotateAxisX = true;
+			break;
+		case GLUT_KEY_LEFT:
+			rotateAngle -= 10 * 0.11;
+			rotateAxisX = false;
+			break;
+		case GLUT_KEY_RIGHT:
+			rotateAngle += 10 * 0.11;
+			rotateAxisX = false;
+			break;
+	}
+}
+
 void myReshape(int w,int h)
 {
 	glViewport(0,0,w,h);
@@ -119,6 +153,7 @@ void draw_WhiteArea()
 		glVertex3f(0.0f,0.3f,-1.5f);
 	glEnd();
 }
+
 void myinit()
 {
 	glEnable(GL_DEPTH_TEST);
@@ -133,14 +168,27 @@ void myinit()
 	draw_WhiteArea();
 	glEndList();
 }
+
 void drawScene()
 {
 	float j;
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glRotatef(-angle, 0.0f, 1.0f, 0.0f);
+	
+	gluLookAt(0.0, 1.0, 0.0, 0.0, 0.0, -2.0, 0.0, 1.0, 0.0);
+	
+	depth -= 0.003f;
+	
+	
+	if (rotateAxisX == true) {
+		glRotatef(rotateAngle, 1.0f, 0.0f, 0.0f);
+	} else {
+		glRotatef(rotateAngle, 0.0f, 1.0f, 0.0f);
+	}
+	
 	glTranslatef(-4*1.5, 0.0, 4*1.5);
+	
 	for(j=0.0;j>(-8*1.5);j-=1.5)
 	{
 		k++;
@@ -185,6 +233,7 @@ void drawScene()
 	}
 	PawnDisplay();
 	glutSwapBuffers();
+	glutPostRedisplay();
 }
 
 
@@ -199,6 +248,50 @@ void update(int value)
 	glutTimerFunc(25, update, 0);
 }
 
+
+/*MOUSE EVENT*/
+void mouseEvent(int button, int state, int x, int y) 
+{
+	mouseButton = button;
+	switch(button) {
+		case GLUT_LEFT_BUTTON:
+			if (state == GLUT_DOWN) {
+				oldX = x;
+				oldY = y;
+			}
+			break;
+		case GLUT_RIGHT_BUTTON:
+			break;
+		case GLUT_MIDDLE_BUTTON:
+			break;
+	}	
+}
+
+void mouseMotion(int x, int y)
+{
+	if (mouseButton == GLUT_LEFT_BUTTON) {
+		if (abs(x - oldX) > 0 && abs(x - oldX) > abs(y - oldY)) {
+			if ((x - oldX) > 0) {
+				rotateAngle += 10.0 * 0.11;
+			} else {
+				rotateAngle -= 10.0 * 0.11;
+			}
+			
+			rotateAxisX = false;
+		} else if (abs(y - oldY) > 0 && abs(y - oldY) > abs(x - oldX)) {
+			if ((y - oldY) > 0) {
+				rotateAngle += 10.0 * 0.11;
+			} else {
+				rotateAngle -= 100 * 0.11;
+			}
+			rotateAxisX = true;
+		}
+		oldX = x;
+		oldY = y;
+	}
+}
+
+
 int main(int argc,char** argv)
 {
 	glutInit(&argc,argv);
@@ -210,8 +303,13 @@ int main(int argc,char** argv)
 	glutFullScreen();
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(Keypress);
+	glutSpecialFunc(SpecialFuncKey);
 	glutReshapeFunc(myReshape);
 	//glutTimerFunc(25, update, 0);
+	
+	glutMouseFunc(mouseEvent);
+	glutMotionFunc(mouseMotion);
+	
 	glutMainLoop();
 	return 0;
 }
